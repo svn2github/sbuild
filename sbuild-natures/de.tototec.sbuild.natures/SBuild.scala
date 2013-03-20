1208ddfe3dc9b89dc5411b67bbaf971355db23a8
TobiasRoeser <TobiasRoeser@024a02f7-42f7-40fc-9f58-65666d1f2450>
2013-03-20 21:23:00 +0000

    Changed build to be a standard SBuild build.
    git-svn-id: http://sbuild.tototec.de/svn/sbuild/trunk@462 024a02f7-42f7-40fc-9f58-65666d1f2450

--- sbuild-natures/de.tototec.sbuild.natures/SBuild.scala
+++ sbuild-natures/de.tototec.sbuild.natures/SBuild.scala
@@ -2,48 +2,61 @@ import de.tototec.sbuild._
 import de.tototec.sbuild.TargetRefs._
 import de.tototec.sbuild.ant._
 import de.tototec.sbuild.ant.tasks._
+import de.tototec.sbuild.addons.scala.Scalac
 
-import de.tototec.sbuild.natures.experimental._
+@version("0.4.0")
+@classpath("mvn:org.apache.ant:ant:1.9.0")
+class SBuild(implicit _project: Project) {
 
-@version("0.3.1") 
-// Lets just use all the natures of this project directly :-)
-@include(
-  "Natures-Snapshot-201301191524.scala"
-)
-@classpath(
-  "http://repo1.maven.org/maven2/org/apache/ant/ant/1.8.4/ant-1.8.4.jar"
-)
-class SBuild(implicit project: Project) {
+  val scalaVersion = "2.10.1"
+  val sbuildVersion = "0.4.0"
 
-  SchemeHandler("mvn", new MvnSchemeHandler())
-  SchemeHandler("http", new HttpSchemeHandler())
-  SchemeHandler("zip", new ZipSchemeHandler())
-
-  val scalaVersion = "2.10.0"
-  val sbuildVersion = "0.3.1.9000"
-  val sbuildNaturesVersion = "0.0.0.9000"
+  val jar = "target/de.tototec.sbuild.natures-0.0.0.9002.jar"
 
   val compileCp =
     s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
-      s"../../sbuild/de.tototec.sbuild/target/de.tototec.sbuild-${sbuildVersion}.jar" ~
-      s"../../sbuild/de.tototec.sbuild.ant/target/de.tototec.sbuild.ant-${sbuildVersion}.jar" ~
-      s"../../sbuild/de.tototec.sbuild.addons/target/de.tototec.sbuild.addons-${sbuildVersion}.jar" ~
+      s"http://sbuild.tototec.de/sbuild/attachments/download/58/de.tototec.sbuild-${sbuildVersion}.jar" ~
+      s"http://sbuild.tototec.de/sbuild/attachments/download/59/de.tototec.sbuild.addons-${sbuildVersion}.jar" ~
+      s"http://sbuild.tototec.de/sbuild/attachments/download/60/de.tototec.sbuild.ant-${sbuildVersion}.jar" ~
       "mvn:org.apache.ant:ant:1.8.4"
 
   ExportDependencies("eclipse.classpath", compileCp)
 
-  val tAll = Target("phony:all") help "Default target: Build all"
-
-  val nat1 = new CleanNature with CompileScalaNature with JarNature with ScalaSourcesNature {
-
-    override def artifact_name = "de.tototec.sbuild.natures.experimental"
-    override def artifact_version = sbuildNaturesVersion
-    override def compileScala_compileClasspath = compileCp
-    override def jar_dependsOn = compileScala_targetName
-
+  Target("phony:clean").evictCache exec {
+    AntDelete(dir = Path("target"))
   }
 
-  tAll dependsOn nat1.jar_output
-  nat1.createTargets
+  Target("phony:all") dependsOn "jar"
+  Target("phony:jar") dependsOn jar
+
+  val compilerClasspath =
+    s"mvn:org.scala-lang:scala-library:${scalaVersion}" ~
+    s"mvn:org.scala-lang:scala-compiler:${scalaVersion}" ~
+    s"mvn:org.scala-lang:scala-reflect:${scalaVersion}"
+
+
+  Target("phony:compile").cacheable dependsOn compileCp ~ "scan:src/main/scala" ~ compilerClasspath exec {
+    Scalac(
+      compilerClasspath = compilerClasspath.files,
+      classpath = compileCp.files,
+      sources = "scan:src/main/scala".files,
+      destDir = Path("target/classes"),
+      debugInfo = "vars"
+    )
+  }
+
+  Target(jar) dependsOn "compile" exec { ctx: TargetContext =>
+    AntJar(destFile = ctx.targetFile.get, baseDir = Path("target/classes"))
+  }
+
+//  val nat1 = new CleanNature with CompileScalaNature with JarNature with ScalaSourcesNature {
+//    override def artifact_name = "de.tototec.sbuild.natures.experimental"
+//    override def artifact_version = sbuildNaturesVersion
+//    override def compileScala_compileClasspath = compileCp
+//    override def jar_dependsOn = compileScala_targetName
+//  }
+
+//  tAll dependsOn nat1.jar_output
+//  nat1.createTargets
 
 }
